a846116374f2e63404b5d0500514e8302ab085a7
Akash Shingha <akashshingha850@gmail.com>
2024-09-20 17:28:20 +0300

    Initial commit for rtsp-stream

--- stream-object.sh
+++ stream-object.sh
@@ -9,12 +9,15 @@ while true; do
     #./detectnet --ssl-key=key.pem --ssl-cert=cert.pem rtsp://192.168.144.108:554/live webrtc://@:8555/object || true
     #video-viewer rtsp://192.168.0.100:8554/uavcast || true
     
-	detectnet --output-save=media/output_$(date +"%Y%m%d_%H%M%S").mp4 --ssl-key=key.pem --ssl-cert=cert.pem rtsp://192.168.144.108:554/live webrtc://192.168.192.100:8556/output || true
+	detectnet \
+	--output-save=/home/jetson/vtt/media/object_$(date +"%Y%m%d_%H%M%S").mp4 \
+	--ssl-key=key.pem --ssl-cert=cert.pem \
+	rtsp://192.168.144.108:554/live webrtc://192.168.192.100:8556/output || true
     #./detectnet --ssl-key=key.pem --ssl-cert=cert.pem rtsp://192.168.144.108:554/live webrtc://192.168.192.100:8556/output || true
     
     #detectnet.py rtsp://192.168.192.101:8554/uavcast  --output-save=~/vtt/media/Video_$(date +"%Y%m%d_%H%M%S").mp4
     
     #--input-save=/media/camera_dump.mp4 --output-save=/media/post_dump.mp4 
     echo "Streaming  stopped. Restarting..."
-    exit
+    #exit
 done
